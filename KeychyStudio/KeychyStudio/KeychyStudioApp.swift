53cdac5afbd8b092420501112cfff43c43e3107e
giljihun <accc45@outlook.com>
2026-03-18 15:48:04 +0900

    chore: Firebase 기본 설정

---
+++ KeychyStudio/KeychyStudio/KeychyStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychyStudioApp.swift
+//  KeychyStudio
+//
+//  Created by 길지훈 on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct KeychyStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
