eec235158ec179e7b96f1925daced9823d2436a9
giljihun <accc45@outlook.com>
2026-03-19 17:18:48 +0900

    chore: MVVM 구조화

---
+++ KeychyStudio/KeychyStudio/Presentation/Auth/ViewModels/AuthViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AuthViewModel.swift
+//  KeychyStudio
+//
+//  Created by 길지훈 on 2026-03-18.
+//
+
+import Foundation
+
+@MainActor
+@Observable
+final class AuthViewModel {
+
+}
---
+++ KeychyStudio/KeychyStudio/Presentation/Auth/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  KeychyStudio
+//
+//  Created by 길지훈 on 2026-03-18.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("LoginView")
+    }
+}
+
+#Preview {
+    LoginView()
+}
